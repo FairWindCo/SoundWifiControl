b393a8271f93bbd79bc6fa4a2be20003ea964384
FairWindCo <sergey.manenok@gmail.com>
2016-02-09 13:44:20 +0200

    Initial commit

---
+++ app/src/main/java/ua/pp/fairwind/soundwificontrol/ProfileActionCreator.java
@@ -0,0 +1,14 @@
+package ua.pp.fairwind.soundwificontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActionCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_action_creator);
+        
+    }
+}
